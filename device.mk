a8d0093afeac028c694ac4039b08b36a654e96c8
GtrCraft <nickvanbokhorst@hotmail.com>
2020-11-06 14:58:12 +0100

    surya: Add lineage overlays
    Change-Id: I622e8cca0e1a133ca83d9a42ba08838fed109cd3

--- device.mk
+++ device.mk
@@ -96,7 +96,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Power
 PRODUCT_PACKAGES += \
