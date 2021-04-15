88cc7c1573426a0136f1b9376b50c129554f2add
GtrCraft <nickvanbokhorst@hotmail.com>
2021-04-15 08:36:51 +0200

    Revert "juice: remove packages on build time"
    This reverts commit e4e75c282e8d7d392525db8719bd8230e960669b.

--- device.mk
+++ device.mk
@@ -99,10 +99,6 @@ PRODUCT_PACKAGES += \
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
